f8bb3f0a622a7d98a626c94989c414eeaafebe75
jrobinson57 <goalie101010@gmail.com>
2024-05-01 18:10:54 -0400

    css styling adjustments

--- client/src/Contact.jsx
+++ client/src/Contact.jsx
@@ -1,43 +1,42 @@
-import React, { useEffect, useState } from "react";
-import {Link} from "react-router-dom";
-import axios from "axios";
-import './Contact.css';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from './Contact.module.css';
 
 function Contact() {
-    
     return (
-        <div className='container'>
-            <div className='navbar'>
-                <div className='navbar-left'>
+        <div className={styles.pageBackground}>
+            <div className={styles.navbar}>
+                <div className={styles.navbarLeft}>
                     <h2>Rowan Web Programming</h2>
                 </div>
-                <div className='navbar-right'>
-                    <Link to={`/`} className='btn btn-warning'>Home</Link>
-                    <button>About</button>
-                    <button>Contact Us</button>
+                <div className={styles.navbarRight}>
+                    <Link to={`/`} className={styles.btn}>Home</Link>
+                    <Link to={`/about`} className={styles.btn}>About</Link>
+                    <Link to={`/contact`} className={styles.btn}>Contact Us</Link>
                 </div>
             </div>
-            <div className='image-section'>
-                <img src='/movietheater.jpg' alt='Background' />
-                <div className='overlay'>
-                    <div className='overlay-content'>
-                        <h1>Contact Us</h1>
-                        <p>Responsive Movie Rating App</p>
-                        <p></p>
-                        <h2>James Robinson</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt faucibus justo, 
-                            tincidunt semper neque euismod sollicitudin. Pellentesque interdum odio quis lobortis vehicula. 
-                            Maecenas dictum felis quis nulla convallis imperdiet. Phasellus aliquet efficitur lacinia. In ornare ullamcorper massa. 
-                            Donec finibus eu quam nec feugiat. Ut tincidunt interdum mauris, vel placerat ipsum varius ac. Maecenas ut erat leo. 
-                            Morbi finibus nisi vel euismod dictum. Praesent pretium, odio et vehicula mollis, ante dui accumsan dolor, sit amet maximus justo erat mollis metus. </p>
-                        
 
+            <div className={styles.imageSection}>
+                
+                <div className={styles.overlay}>
+                    <div className={styles.overlayContent}>
+                        <h1 className={styles.contactTxt}>Contact Us</h1>
+                        <p className={styles.responsiveTxt}>Responsive Movie Rating App</p>
+                        <h2>James Robinson</h2>
+                        <p>Thank you so much for using our Movie Rating Tracker Webapp! If you have any questions or just want to reach out to us, please
+                            contact the company at info@movieratingtracker.com!
+                        </p>
                     </div>
                 </div>
             </div>
+            
+            <footer className={styles.footer}>
+                <div className={styles.footerContent}>
+                    &copy; 2024 Rowan Web Programming
+                </div>
+            </footer>
         </div>
-    )
+    );
 }
 
 export default Contact;
--- client/src/Movies.jsx
+++ client/src/Movies.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import styles from "./Movies.module.css"; // Import CSS module
+import styles from "./Movies.module.css"; 
 
 function Movies() {
     axios.defaults.withCredentials = true;
